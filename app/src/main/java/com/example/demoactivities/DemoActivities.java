57934aecb2989062a22c29e619ef932d25ec8c69
Kriaeyl <osireikri@gmail.com>
2020-04-23 12:24:08 +0800

    done!

--- app/src/main/java/com/example/demoactivities/DemoActivities.java
+++ app/src/main/java/com/example/demoactivities/DemoActivities.java
@@ -2,7 +2,11 @@ package com.example.demoactivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class DemoActivities extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class DemoActivities extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_demo_activities);
+
+        Button btnDone = findViewById(R.id.button);
+        btnDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText etName = findViewById(R.id.editText);
+                EditText etAge = findViewById(R.id.editText2);
+                String[] info = {etName.getText().toString(), etAge.getText().toString()};
+                Intent i = new Intent(DemoActivities.this, DemoActivities2.class);
+                i.putExtra("info", info);
+                startActivity(i);
+            }
+        });
     }
 }
